f3a64b01b931753a13ee801979a0d9d0bd783f64
sahil19896 <sahil19896@gmail.com>
2025-12-31 18:52:02 +0000

    job page fixes

--- tft_webportal/www/job_list.py
+++ tft_webportal/www/job_list.py
@@ -19,13 +19,13 @@ def load_my_jobs(limit):
 			
 		# Fetch jobs for the current users
 		filters = {"supplier": ["IN", users]}
-		fields=["name", "p_o_number", "date", "status"]
+		fields=["name", "p_o_number", "date", "status", "p_o_number", "from_address", "to_address"]
 		jobs = frappe.db.get_list("Transportation Request", filters, fields, ignore_permissions=True, limit=limit)
-		for job in jobs:
-			doc = frappe.get_doc("Transportation Request", job.name, ignore_permissions=True)
-			job['from_address'] = doc.get("addresses")[0].from_address if doc.get("addresses", []) else ""
-			job['to_address'] = doc.get("addresses")[0].to_address if doc.get("addresses", []) else ""
-			job['vehicles'] = len(doc.get("vehicle")) or 0
+		# for job in jobs:
+		# 	doc = frappe.get_doc("Transportation Request", job.name, ignore_permissions=True)
+		# 	job['from_address'] = doc.get("addresses")[0].from_address if doc.get("addresses", []) else ""
+		# 	job['to_address'] = doc.get("addresses")[0].to_address if doc.get("addresses", []) else ""
+		# 	job['vehicles'] = len(doc.get("vehicle")) or 0
 		return jobs
 	except Exception as e:
 		frappe.error_log("Error loading jobs", str(e))
